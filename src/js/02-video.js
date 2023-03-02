a65f0ead124524042928c40aafc26fc74c723995
nikkuts <nickkuts888@gmail.com>
2023-03-02 20:08:15 +0200

    зміни

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,37 @@
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!')
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title)
-    });
+   
+        import Player from '@vimeo/player';
+       const iframe = document.querySelector('iframe');
+       const player = new Player(iframe);
+   
+       player.on('play', function() {
+           console.log('played the video!');
+       });
+   
+       player.getVideoTitle().then(function(title) {
+           console.log('title:', title);
+       });
+  
+       
+       refreshPage();
 
     const onPlay = function({
         duration,
         percent,
         seconds
     }) {
-        // data is an object containing properties specific to that event
+        localStorage.setItem("videoplayer-current-time", seconds);
+        console.log(localStorage);
     };
     
     player.on('timeupdate', onPlay);
+
+    const refreshPage = function() {
+        let currentTime = localStorage.getItem("videoplayer-current-time");
+
+        if (currentTime) {
+            player.setCurrentTime(currentTime);
+            localStorage.removeItem("videoplayer-current-time");
+        }
+    };
+
+   
