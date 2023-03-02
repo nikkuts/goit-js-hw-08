347bcec655b0be106ffd0de443d1f1fd23d9113b
nikkuts <nickkuts888@gmail.com>
2023-03-02 16:01:07 +0200

    оновлення2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
-const newConstant;
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!')
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title)
+    });
+
+    const onPlay = function({
+        duration,
+        percent,
+        seconds
+    }) {
+        // data is an object containing properties specific to that event
+    };
+    
+    player.on('timeupdate', onPlay);
